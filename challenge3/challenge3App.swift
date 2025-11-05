6fd568d0c0b33a28cec8ecf371bc5da2191c2011
Lai Hong Yu <joyjay606@gmail.com>
2025-11-05 16:10:00 +0800

    Initial Commit

---
+++ challenge3/challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge3App.swift
+//  challenge3
+//
+//  Created by Lai Hong Yu on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
