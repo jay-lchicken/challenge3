3184c9555124fbe6880736638fc14af821f03edd
Lai Hong Yu <joyjay606@gmail.com>
2025-11-07 11:19:10 +0800

    added tab bars

--- challenge3/ContentView.swift
+++ challenge3/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentTab: Int = 0
+    @State var showSheet = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(){
+            
+            
+            Tab("Home", systemImage: "house"){
+                Text("home")
+            }
+
+            Tab("Finance", systemImage: "bitcoinsign.circle"){
+                Text("finance")
+            }
+
+            Tab("Add", systemImage: "plus", role: .search) {
+                Text("add")
+            }
         }
-        .padding()
+        .onChange(of: currentTab) { newValue, oldValue in
+            if newValue == 2 {
+                showSheet = true
+                currentTab = oldValue
+            }
+        }
+        .tabBarMinimizeBehavior(.onScrollDown)
+        .tabViewBottomAccessory {
+            TextField("What would you like to do today?", text: .constant(""))
+                .textFieldStyle(.plain)
+                .padding()
+                
+        }
     }
 }
 
